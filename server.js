b8d720b688796e1add3a2a30af614b8238a12dbc
daddehs1 <sdaddeh2@gmail.com>
2019-04-07 04:02:22 -0700

    add catch-all path for history-mode routing

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+app.use("/", serveStatic(path.join(__dirname, '/dist')))
+// Catch all routes and redirect to the index file
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+// Create default port to serve the app on
+const port = process.env.PORT || 5000
+app.listen(port)
